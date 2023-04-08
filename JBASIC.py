b88efbfc1834323d78e789c954c5c5451965f02e
Dilberto <dilbertoms@gmail.com>
2023-04-07 23:51:21 -0300

    Diretorio Musicas

--- JBASIC.py
+++ JBASIC.py
@@ -5,8 +5,7 @@ from random import randint
 
 pygame.init()
 
-musicaf = pygame.mixer.music.load(
-    "Musicas/X2Download.app - Damassaclan _ Chayco _Contramão_ (Prod. NeoBeats) (128 kbps).mp3")
+musicaf = pygame.mixer.music.load("Musicas/X2Download.app - Damassaclan _ Chayco _Contramão_ (Prod. NeoBeats) (128 kbps).mp3")
 pygame.mixer.music.play(-1)
 
 somp = pygame.mixer.Sound("Musicas/smw_1-up.wav")
